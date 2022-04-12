b931df7f97d8c0d2e79dff08c9104c68e0eecbc5
Adetola Fadairo <adetolafadairo@gmail.com>
2022-04-12 03:50:13 +0000

    adding ectra fields from db miugrate to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220409193307) do
+ActiveRecord::Schema.define(version: 20220412021124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20220409193307) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "last_name"
+    t.string   "first_name"
   end
 
   add_foreign_key "line_items", "orders"
