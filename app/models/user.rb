87f0b3c0f84a295473f599ec339f6f8efbba0031
Adetola Fadairo <adetolafadairo@gmail.com>
2022-04-12 03:52:02 +0000

    adding validations to file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, case_sensitive: true, presence: true
+  validates :password, length: { minimum: 4}, on: :create
 
 end
